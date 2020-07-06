1e53522da7bb53239ff4d524f9886a96a0aede92
Amandine Théry <amandine.thery@prowebce.com>
2020-07-06 12:44:22 +0200

    Premier commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+
+    </div>
+  );
+}
+
+export default App;
