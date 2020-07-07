a932709a06f2db53c49f2d92f897d46d91a807bb
Amandine Théry <amandine.thery@prowebce.com>
2020-07-07 18:50:38 +0200

    ajout de message

--- src/components/FeatureComponents/TchatSender/TchatSender.jsx
+++ src/components/FeatureComponents/TchatSender/TchatSender.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../UIComponents/Button/Button';
 import styles from './TchatSender.module.scss';
+import * as moment from 'moment';
 
-const TchatSender = props => (
-  <div className={styles.TchatSender}>
-    <form>
-      <input type="text" name="messageOutbox" placeholder="Saisissez votre message" />
-      <Button>Env<img src="/img/logo.svg" alt=""/>yer</Button>
-    </form>
-  </div>
-);
+const TchatSender = props => {
+
+  let [messageOutbox, setMessage] = useState('');// hook d'état
+  const submitMessage = (message) => {
+    let myHeaders = new Headers();
+    message.heure = moment().format('YYYYMMDDDHHmmss');
+    myHeaders.append("Content-Type", "application/json");
+    fetch('http://localhost:4456/messages', { 
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(message)
+    }).then(e=>e.json).then(e=>{console.log(e);setMessage('')});
+  }
+
+  return (
+    <div className={styles.TchatSender}>
+      <form onSubmit={e=>{
+          e.preventDefault();
+          submitMessage({userId: props.nickname, message:messageOutbox});
+      }}>
+        <input type="text" name="messageOutbox" value={messageOutbox} onChange={e => {setMessage(e.currentTarget.value)}} placeholder="Saisissez votre message" />
+        <Button onClick={e=>{submitMessage({userId: props.nickname, message:messageOutbox})}}>Env<img src="/img/logo.svg" alt=""/>yer</Button>
+      </form>
+    </div>
+  );
+}
 
 TchatSender.propTypes = {
   nickname: PropTypes.string,
